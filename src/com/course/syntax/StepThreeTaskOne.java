c54da542c55d68dd5aa418f4ec283092f7ccdf5b
Karyna-Adzhyaliieva <karina.knu97@gmail.com>
2023-04-12 18:46:09 +0200

    Step three task one

---
+++ src/com/course/syntax/StepThreeTaskOne.java
@@ -0,0 +1,20 @@
+package com.course.syntax;
+import java.util.Scanner;
+public class StepThreeTaskOne {
+    public static void main(String[] args) {
+        System.out.print("Введите число");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        System.out.println ("Вы ввели число " + number);
+
+        if (number < 10 && number != 0) {
+            System.out.println("меньше 10");
+        }
+        else if (number > 10) {
+            System.out.println("больше 10");
+        }
+        else if (number == 0) {
+            System.out.println("равно 0");
+        }
+    }
+}
