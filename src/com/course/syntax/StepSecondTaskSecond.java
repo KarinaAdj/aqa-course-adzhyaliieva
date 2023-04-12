1d1e36308bc6988ab5f91af9dd1e6abcf8458b93
Karyna-Adzhyaliieva <karina.knu97@gmail.com>
2023-04-12 18:43:33 +0200

    Step second task second

---
+++ src/com/course/syntax/StepSecondTaskSecond.java
@@ -0,0 +1,13 @@
+package com.course.syntax;
+
+public class StepSecondTaskSecond {
+    public static void main(String[] args) {
+        int n = 3;
+        n++;
+        System.out.println(n);
+        n++;
+        System.out.println(n);
+        n--;
+        System.out.println(n);
+    }
+}
