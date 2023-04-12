bbed97e353c6a8986108f878c40209f1b21a1099
Karyna-Adzhyaliieva <karina.knu97@gmail.com>
2023-04-12 18:31:19 +0200

    Step first

---
+++ src/com/course/syntax/StepFirst.java
@@ -0,0 +1,16 @@
+package com.course.syntax;
+
+public class StepFirst {
+    public static void main(String[] args) {
+        byte byteVarName = 15;
+        short shorVarName = 333;
+        int intVarName = 26;
+        long longVarName = 21470;
+        float floatVarName = 20.55f;
+        double doubleVarName = 10.20;
+        char charVarName = 'a';
+        boolean booleanVarName = true;
+
+        System.out.println("The age " + longVarName + " is not valid, but " + intVarName + " is valid" );
+    }
+}
