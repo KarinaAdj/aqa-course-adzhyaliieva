226d1beb6bda40cf7ca3f2ce87ea8980a8eae4a3
Karyna-Adzhyaliieva <karina.knu97@gmail.com>
2023-04-16 11:43:30 +0200

    done

---
+++ src/com/course/syntax/StepFiveTaskOne.java
@@ -0,0 +1,15 @@
+package com.course.syntax;
+
+public class StepFiveTaskOne {
+    // calculate average value of given array with numbers and print this value to the console.
+    // Program must calculate average value for array with any number of elements
+    public static void main(String[] args) {
+        int [] array = {2,1,35,2};
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        System.out.println(sum / array.length);
+    }
+}
